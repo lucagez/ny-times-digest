97e8cbd74b338821d852e539d1c4559d9f2a1240
Luca Gesmundo <lucagesmundo@yahoo.it>
2018-06-01 14:21:57 +0200

    Auto stash before merge of "master" and "ny-times-digest/master"

---
+++ src/components/NYloader.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const NYloader = () => {
+    return(
+        <div className="NYloader"><h1 style={{color: '#292929'}}>loading your digest...</h1></div>
+    ) 
+}
+
+export default NYloader;
